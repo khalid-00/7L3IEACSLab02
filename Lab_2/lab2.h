0ccd2e1433f3a3ad8e1df1dad37f58a75af910bf
Khalid Alshikh <Khalid.alshikh9@gmail.com>
2020-04-17 19:27:51 +0200

    Mohammad Khaled Alshikh

---
+++ Lab_2/lab2.h
@@ -0,0 +1,13 @@
+#ifndef LAB2_H
+#define LAB2_H
+#include <cstring>
+#include <string>
+#include <iostream>
+
+
+std::string encrypt(std::string , int );
+std::string decencrypt(std::string , int );
+std::string generateKey(std::string , std::string);
+std::string cipherText(std::string , std::string);
+std::string originalText(std::string , std::string);
+#endif // LAB2_H
---
+++ Lab_2/tessttt.pro
@@ -0,0 +1,14 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        main problem2.cpp \
+        prblem1.cpp \
+        prblem2.cpp \
+        problem1main.cpp \
+        problem2main.cpp
+
+HEADERS += \
+    lab2.h
